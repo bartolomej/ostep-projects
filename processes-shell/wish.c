66c49b967d2ef0d4df6f08dfbd6c31ec298c70a0
Bart <bartolomej.kozorog@gmail.com>
2023-05-29 09:50:08 +0200

    add repl loop

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -6,21 +6,32 @@
 int main(int argc, char *argv[])
 {
 
-    char* line = NULL;
+    char *line = NULL;
     size_t line_length = 0;
-
-    int bytes_read = getline(&line, &line_length, stdin);
-
     Command parsed_command;
 
-    init_command(&parsed_command);
-    parse_command(&parsed_command, line);
-
-    printf("Command path: %s\n", parsed_command.path);
-    printf("Command argc: %d\n", parsed_command.argc);
-
-    for (int i = 0; i < MAX_PARAMETERS; i++)
+    while (1)
     {
-        printf("Argument %d: %s\n", i, parsed_command.argv[i]);
+        printf("> ");
+        int bytes_read = getline(&line, &line_length, stdin);
+
+        init_command(&parsed_command);
+        parse_command(&parsed_command, line);
+
+        int child_pid = fork();
+
+        if (child_pid < 0)
+        {
+            fprintf(stderr, "fork failed\n");
+            exit(1);
+        }
+        else if (child_pid == 0)
+        {
+            execvp(parsed_command.path, parsed_command.argv);
+        }
+        else
+        {
+            wait(&child_pid);
+        }
     }
 }
